55790d9499c0e789562705ce8d2394aa12d92881
EL GARTI YASSINE <yass.elgarti@gmail.com>
2025-12-03 16:42:02 +0100

    test

--- taxibiker-back/config/bundles.php
+++ taxibiker-back/config/bundles.php
@@ -1,14 +1,23 @@
 <?php
 
-return [
+$bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
+
+// Bundles uniquement en dev/test, chargés uniquement s'ils sont disponibles
+if (class_exists(\Symfony\Bundle\MakerBundle\MakerBundle::class)) {
+    $bundles[\Symfony\Bundle\MakerBundle\MakerBundle::class] = ['dev' => true];
+}
+
+if (class_exists(\Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class)) {
+    $bundles[\Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class] = ['dev' => true, 'test' => true];
+}
+
+return $bundles;
